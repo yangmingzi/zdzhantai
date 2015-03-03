e711a560f179b2fe8027f24ef95164144061c98a
yangmingzi <120869049@qq.com>
2015-03-03 11:23:40 +0800

    commit

---
+++ 20150211/MainWindow.xaml.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Threading;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Net;
+using System.Data.SQLite;
+using Dell;
+using System.IO;
+namespace zdzhantai
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            initSqlite.initUpdateTable();
+            initSqlite.initUsingTable();
+            Update();         
+            InitializeComponent();            
+        }
+      
+        public static void  Update()
+        {
+            int j = updateSq.checkLastUpdate();
+            Thread t = new Thread(updateSq.UpdateFromServer);
+            t.Start();
+        }
+        private void Button_Click_image(object sender, RoutedEventArgs e)
+        {
+            img_play win = new img_play();
+            win.Show();
+
+        }
+        private void Button_Click_aboutZd(object sender, RoutedEventArgs e)
+        {
+            about_cp win = new about_cp();
+            win.Show();
+        }
+
+       
+        private void Button_Click_Close (object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+      
+
+        private void Button_Click_trace(object sender, RoutedEventArgs e)
+        {
+            trace win = new trace();
+            win.Show();
+        }
+
+        //public static Ops bulidOp()
+        //{
+        //    string dbPath = "Data Source =" + Environment.CurrentDirectory + "/test.db";
+        //    Ops Op = new Ops(dbPath);
+        //    return Op;
+        //}
+        //public static void UpdateFromServer()
+        //{
+        //    //string dbPath = "Data Source =" + Environment.CurrentDirectory + "/test.db";
+        //    string filePath;
+        //    Ops Op = bulidOp();
+
+        //    string cmdInsertText1 = "INSERT INTO fileUpdate VALUES(@type,@url,@filename,@filepath,@state)";
+           
+
+        //    //// 数组  【】    对象{}     值“”
+        //    string json1 = "{type:'image',url:'http://i1.hoopchina.com.cn/blogfile/201501/08/BbsImg142070525332424_550*550.jpg'}";
+        //    string json2 = "{type:'video',url:'http://i2.hoopchina.com.cn/blogfile/201411/27/BbsImg141707487288557_1271*988.jpg'}";
+        //    //string json2 = "{type:'video',url:'http://v.youku.com/v_show/id_XODczMTA2OTc2.html'}";
+        //    string json3 = "{type:'image',url:'http://i1.hoopchina.com.cn/blogfile/201304/09/136548593327743.jpg'}";
+        //    string jsonString = "[" + json1 + "," + json2 + "," + json3 + "]";
+
+
+        //    //Ops Op = new Ops(dbPath);
+
+        //    StringBuilder sb = new StringBuilder();
+        //    JArray array1 = (JArray)JsonConvert.DeserializeObject(jsonString);
+        //    for (int i = 0; i < 3; ++i)
+        //    {
+        //        WebClient mywebclient = new WebClient();
+        //        JObject jo = (JObject)array1[i];
+        //        string url = jo["url"].ToString();
+        //        string newfilename = DateTime.Now.Year.ToString() + DateTime.Now.Month.ToString()
+        //            + DateTime.Now.Day.ToString() + DateTime.Now.Hour.ToString() + DateTime.Now.Minute.ToString() + DateTime.Now.Second.ToString() + i + ".jpg";
+        //        //下载很快，如果不在文件名加入i的话，会在一秒内下载完，使用时间当做文件名，最后就只剩一个文件。
+        //        //string filePath = @"D:\VSPROJECT\WindowsFormsApplication3\" + newfilename;
+
+        //        if (jo["type"].ToString().Equals("image"))
+        //            filePath = @Environment.CurrentDirectory + "/Image/" + newfilename;
+        //        else if (jo["type"].ToString().Equals("video"))
+        //            filePath = @Environment.CurrentDirectory + "/Video/" + newfilename;
+        //        else
+        //            filePath = @"D:\";
+        //        try
+        //        {
+        //            mywebclient.DownloadFile(url, filePath);
+        //            SQLiteParameter[] Params1 = new SQLiteParameter[] {
+        //                                                       new SQLiteParameter("@type", jo["type"]),
+        //                                                       new SQLiteParameter("@url", jo["url"]),
+        //                                                       new SQLiteParameter("@filename", newfilename),
+        //                                                       new SQLiteParameter("@filepath", filePath),
+        //                                                       new SQLiteParameter("@state", 1) };
+
+        //            if (File.Exists(filePath))
+        //            {
+        //                Op.PrepareCommand(cmdInsertText1, Params1);
+        //            }
+
+        //        }
+        //        catch (Exception ex)
+        //        {
+        //            Console.WriteLine(ex.ToString());
+        //        }
+        //    }
+           
+        //    int j = updateSq.checkLastUpdate();
+
+        //}
+
+       
+      
+
+    }
+}
---
+++ 20150211/about_cp.xaml.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace zdzhantai
+{
+    /// <summary>
+    /// about_cp.xaml 的交互逻辑
+    /// </summary>
+    public partial class about_cp : Window
+    {
+        ObservableCollection<BitmapImage> bmList;
+        int index = 0;
+        public about_cp()
+        {
+            InitializeComponent();
+            InitList();
+            btn1.Click += new RoutedEventHandler(Button_Click_1);
+            btn2.Click += new RoutedEventHandler(Button_Click_2);
+            btn3.Click += new RoutedEventHandler(Button_Click_3);
+        }
+
+
+        List<string> list = new List<string>();
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (index > 0)
+            {
+                index--;
+                img_show(bmList[index]);
+            }
+            else
+            {
+                index = bmList.Count - 1;
+                img_show(bmList[index]);
+            }
+        }
+
+        public void img_show(BitmapImage img)
+        {
+            this.Background = new ImageBrush(img);
+        }
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            if (index < (bmList.Count))
+            {
+                img_show(bmList[index]);
+                index++;
+            }
+            else
+            {
+                index = 0;
+                img_show(bmList[index]);
+            }
+        }
+
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+
+
+        public void InitList()
+        {
+            //list = System.IO.Directory.GetFiles(".../.../Image/").ToList();
+            string mulu = Environment.CurrentDirectory.ToString() + "/zdimg";
+            list = System.IO.Directory.GetFiles(mulu).ToList();
+
+            bmList = new ObservableCollection<BitmapImage>();
+            for (int i = 0; i < list.Count; i++)
+            {
+
+                Uri url = new Uri(list[i].Substring(0));
+                BitmapImage bmImg = new BitmapImage();
+                bmImg.BeginInit();
+                bmImg.UriSource = url;
+                bmImg.EndInit();
+                bmList.Add(bmImg);
+            }
+
+        }
+    }
+}
