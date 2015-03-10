d26edc72504c37f2fa19f7cad91e3317b1f64d09
yangmingzi <120869049@qq.com>
2015-03-10 21:12:25 +0800

    commit

--- MouseGesture/TouchGesture/img_play.xaml.cs
+++ MouseGesture/TouchGesture/img_play.xaml.cs
@@ -79,36 +79,11 @@ namespace zdzhantai
                 }
             }
         }
-        private void Button_Click_1(object sender, RoutedEventArgs e)
-        {
-            if (index > 0)
-            {
-                index--;
-                img_show(bmList[index]);
-            }
-            else
-            {
-                index = bmList.Count - 1;
-                img_show(bmList[index]);
-            }            
-        }
+        
 
         public void img_show(BitmapImage img)
         {
             this.Background = new ImageBrush(img);
-        }
-        private void Button_Click_2(object sender, RoutedEventArgs e)
-        {
-            if (index <(bmList.Count))
-            {                            
-                img_show(bmList[index]);
-                index++;
-            }
-            else
-            {
-                index = 0;
-                img_show(bmList[index]);
-            }
         }
        
 
